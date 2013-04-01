4f6169c6dad4eb90a929300c695585b770bb3f1e
Gareth Jones <gareth.jones@sensis.com.au>
2013-04-01 15:51:31 +1100

    changed explosion sound

--- sounds.js
+++ sounds.js
@@ -9,7 +9,7 @@ function SoundEngine() {
   this.sounds = {
     "pew-pew": "assets/zander-noriega-scifi-vol-1/laser_gun_2.wav",
     "bounce": "assets/JBM_Sfxr_pack_1/samples/collect/collect01.wav",
-    "boom": "assets/JBM_Sfxr_pack_1/samples/explosions/small/explosion-1.wav"
+    "boom": "assets/sounds/explosions/6.wav"
   };
   this.soundBuffers = {};
   this.loadSounds();
